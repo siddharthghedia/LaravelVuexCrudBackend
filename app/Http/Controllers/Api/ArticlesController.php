82e8a8883f6a393decf068fde9c2f2578f759582
SomaiyaJinal <jinal@artixun.com>
2017-12-29 16:33:35 +0530

    article api

---
+++ app/Http/Controllers/Api/ArticlesController.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Article;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ArticlesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $articles = Article::all();
+
+        return response()->json($articles);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|string',
+            'description' => 'required|string'
+        ]);
+
+        Article::create([
+            'name' => $request->get('name'),
+            'description' => $request->get('description')
+        ]);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Article added Successfully.'
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Article $article)
+    {
+        return response()->json($article);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Article $article)
+    {
+        $this->validate($request, [
+            'name' => 'required|string',
+            'description' => 'required|string'
+        ]);
+
+        $article->fill([
+            'name' => $request->get('name'),
+            'description' => $request->get('description')
+        ])->save();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Article updated Successfully.'
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Article $article)
+    {
+        $article->delete();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Article deleted Successfully.'
+        ]);
+    }
+}
